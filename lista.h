1d811f40ee0ecdb5af76967a3cc6b70501865f67
cafrada <fcapra@fi.uba.ar>
2021-02-07 18:06:24 -0300

    Agrego los archivos de lista y nodo

---
+++ lista.h
@@ -0,0 +1,198 @@
+//
+// Created by franco on 15/11/20.
+//
+
+#ifndef TP2_LISTA_H
+#define TP2_LISTA_H
+
+#include <string>
+#include "nodo.h"
+#include <iostream>
+
+using namespace std;
+
+template<typename Dato>
+class Lista {
+private:
+    Nodo<Dato> *primero;
+    int largo;
+
+public:
+    /*
+     * PRE:
+     * POST:
+     * FUNCIONAMIENTO:
+     */
+    Lista();
+
+    /*
+     * PRE: -
+     *
+     * POST: -
+     *
+     * FUNCIONAMIENTO: agrega al principio de la lista el elemento .
+     *                  asigna a primero la direccion del elemento y aumenta el largo en 1.
+     */
+    void alta(Dato elemento_agregar);
+
+    /*
+     * PRE: -
+     *
+     * POST: Devuelve la cantidad de elementos que contiene la lista.
+     *
+     * FUNCIONAMIENTO: -
+     */
+    int obtenerLargo();
+
+    /*
+     * PRE: -
+     *
+     * POST: devuelve el dato del elemento encontrado y si no lo encuentra devuelve
+     * un puntero nulo. Si en la lista hay mas de una coincidencia devuelve la primera.
+     *
+     * FUNCIONAMIENTO: -
+     * */
+    Dato buscarElemento(string nombre);
+
+    /*
+     * PRE: posicion mayor a 0 y menor al largo de la lista.
+     * POST: -
+     * FUNCIONAMIENTO: elimina el elemento de la posicion pasada y apunta el anterior al eliminado
+     *                  al siguiente de la lista.
+     */
+    void baja(int posicion);
+
+    /*
+     * PRE: -
+     * POST: Devuelve si la lista tiene elementos.
+     * FUNCIONAMIENTO: -
+     */
+    bool listaVacia();
+
+    /*
+     * PRE: -
+     *
+     * POST: Muestra la informacion de los elementos  de la lista con su respectiva posicion.
+     *
+     * FUNCIONAMIENTO: -
+     */
+    void mostrarLista();
+
+    /*
+     * PRE:
+     * POST:
+     * FUNCIONAMIENTO:
+     */
+    Nodo<Dato> *buscarNodo(int posicion);
+
+    ~Lista();
+};
+
+
+template<typename Dato>
+Lista<Dato>::Lista() {
+
+    primero = nullptr;
+
+    largo = 0;
+}
+
+template<typename Dato>
+int Lista<Dato>::obtenerLargo() {
+    return largo;
+}
+
+template<typename Dato>
+bool Lista<Dato>::listaVacia() {
+    return largo == 0;
+}
+
+
+template<typename Dato>
+void Lista<Dato>::alta(Dato elemento_agregar) {
+    auto *nuevo_nodo = new Nodo<Dato>(elemento_agregar);
+
+    if (largo == 0) {
+        primero = nuevo_nodo;
+    } else {
+        nuevo_nodo->asignarSiguiente(primero);
+        primero = nuevo_nodo;
+    }
+
+    largo++;
+}
+
+
+template<typename Dato>
+Dato Lista<Dato>::buscarElemento(string nombre) {
+
+    Nodo<Dato> *actual = primero;
+
+    while (actual != nullptr && actual->obtenerDato()->obtenerNombre() != nombre) {
+        actual = actual->obtenerSiguiente();
+    }
+
+    if (!actual) {
+        return nullptr;
+    }
+    return actual->obtenerDato();
+}
+
+template<typename Dato>
+void Lista<Dato>::baja(int posicion) {
+
+    Nodo<Dato> *nodo_aux = buscarNodo(posicion);
+
+    if (posicion == 1) {
+        primero = nodo_aux->obtenerSiguiente();
+
+    } else {
+        Nodo<Dato> *nodo_anterior = buscarNodo(posicion - 1);
+
+        nodo_anterior->asignarSiguiente(nodo_aux->obtenerSiguiente());
+    }
+
+    largo--;
+
+    delete nodo_aux;
+}
+
+template<typename Dato>
+void Lista<Dato>::mostrarLista() {
+
+    if (largo == 0) {
+        cout << "\nLa lista esta vacia\n" << endl;
+    } else {
+        for (int i = 1; i <= largo; i++) {
+            Dato elemento = buscarNodo(i)->obtenerDato();
+            cout << "\n--------------------" << endl;
+            cout << "Elemento Nº" << i << ": " << endl;
+            elemento->mostrarDatos();
+        }
+        cout << "--------------------" << endl;
+    }
+}
+
+template<typename Dato>
+Nodo<Dato> *Lista<Dato>::buscarNodo(int posicion) {
+    if (listaVacia() || posicion > largo) {
+        return nullptr;
+    }
+
+    Nodo<Dato> *buscado = primero;
+
+    for (int i = 1; i < posicion; i++) {
+        buscado = buscado->obtenerSiguiente();
+    }
+
+    return buscado;
+}
+
+template<typename Dato>
+Lista<Dato>::~Lista() {
+    while (!listaVacia()) {
+        baja(1);
+    }
+}
+
+#endif //TP2_LISTA_H
---
+++ nodo.h
@@ -0,0 +1,82 @@
+//
+// Created by franco on 15/11/20.
+//
+
+#ifndef TP2_NODO_H
+#define TP2_NODO_H
+
+template<typename Dato>
+class Nodo {
+    //Atributos
+private:
+    Dato elemento;
+
+    Nodo<Dato> *siguiente;
+
+    //Metodos
+public:
+    /*
+     * PRE:
+     * POST:
+     * FUNCIONAMIENTO:
+     */
+    Nodo(Dato elemento_nuevo);
+
+    /*
+     * PRE: -
+     * POST: Devuelve el contenido del nodo.
+     * FUNCIONAMIENTO: -
+     */
+    Dato obtenerDato();
+
+
+    /*
+     * PRE: siguiente_nodo debe ser una direccion de memoria valida.
+     * POST: asigna la direccion a siguiente
+     * FUNCIONAMIENTO: -
+     */
+    void asignarSiguiente(Nodo<Dato> *siguiente_nodo);
+
+
+    /*
+     * PRE: -
+     * POST: Devuelve la direccion del siguiente nodo
+     * FUNCIONAMIENTO: -
+     */
+    Nodo<Dato> *obtenerSiguiente();
+
+    /*
+     * PRE:
+     * POST:
+     * FUNCIONAMIENTO:
+     */
+    ~Nodo();
+};
+
+template<typename Dato>
+Nodo<Dato>::Nodo(Dato elemento_nuevo) {
+    elemento = elemento_nuevo;
+    siguiente = nullptr;
+}
+
+template<typename Dato>
+Dato Nodo<Dato>::obtenerDato() {
+    return elemento;
+}
+
+template<typename Dato>
+Nodo<Dato> *Nodo<Dato>::obtenerSiguiente() {
+    return siguiente;
+}
+
+template<typename Dato>
+void Nodo<Dato>::asignarSiguiente(Nodo<Dato> *siguiente_nodo) {
+    siguiente = siguiente_nodo;
+}
+
+template<typename Dato>
+Nodo<Dato>::~Nodo() {
+    delete elemento;
+}
+
+#endif //TP2_NODO_H
